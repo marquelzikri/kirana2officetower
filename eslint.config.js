3d66c7888a4003d4c3804ff5b04cc6762f03d07a
marquelzikri <zikrimarquel@gmail.com>
2026-07-26 15:50:59 +0700

    chore(eslint): add eslint with typescript max-lines rule set to 250

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      'max-lines': ['error', { max: 250, skipBlankLines: true, skipComments: true }],
+    },
+  },
+  {
+    ignores: ['node_modules/**', 'dist/**', '.wrangler/**', 'uploads/**'],
+  },
+];
